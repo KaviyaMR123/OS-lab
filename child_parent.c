febfc353036ebc44e17269f1698b33449d993bc0
Kaviya MR <148633380+KaviyaMR123@users.noreply.github.com>
2024-07-03 07:41:22 +0530

    Update child_parent.c

--- child_parent.c
+++ child_parent.c
@@ -1,37 +1,25 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
 int main()
 {
-    pid_t pid;
-    int status;
-    pid = fork();
-    if (pid < 0) 
+    int pid;
+    pid=fork();
+    if(pid<0)
     {
-        fprintf(stderr, "Fork failed\n");
-        return 1;
-    } 
-    else if (pid == 0) 
-    {
-        printf("Child process: PID = %d\n", getpid());
-        sleep(3); 
-        printf("Child process exiting\n");
-        return 0;
-    } 
-    else 
-    {
-        printf("Parent process: PID = %d\n", getpid());
-        printf("Parent process waiting for child to exit...\n");
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) 
-        {
-            printf("Child process exited with status %d\n", WEXITSTATUS(status));
-        }
-        else 
-        {
-            printf("Child process exited abnormally\n");
-        }
+        printf("Fork failed");
+        exit(1);
     }
-    return 0;
+    else if(pid==0)
+    {
+        execlp("/bin/date","date",NULL);
+        exit(0);
+    }
+    else
+    {
+        printf("Process id is %d",getpid());
+        wait(NULL);
+        exit(0);
+    }
 }
