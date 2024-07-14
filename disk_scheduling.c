3b9e4de8d68f98c3bc43fd75ffc51ac85826ea09
Kaviya MR <148633380+KaviyaMR123@users.noreply.github.com>
2024-07-14 11:13:57 +0530

    Update disk_scheduling.c

--- disk_scheduling.c
+++ disk_scheduling.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
-int getSeekTime(int a,int b){
+int getSeekTime(int a,int b)
+{
 	if(a>b)
 		return a-b;
 	return b-a;
 }
-
-void copy(int arr[],int temp[],int n){
+void copy(int arr[],int temp[],int n)
+{
 	for(int i=0;i<n;i++)
 		temp[i] = arr[i];
 }
-
-void sort(int arr[],int n){
+void sort(int arr[],int n)
+{
 	int temp;
-	for(int i=0;i<n-1;i++){
-		for(int j=0;j<n-i-1;j++){
-			if(arr[j]>arr[j+1]){
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
 				temp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
@@ -23,19 +26,21 @@ void sort(int arr[],int n){
 		}
 	}
 }
-
-int reqIndex(int arr[],int n,int p){
-	for(int i=0;i<n;i++){
+int reqIndex(int arr[],int n,int p)
+{
+	for(int i=0;i<n;i++)
+	{
 		if(arr[i]>=p)
 			return i;
 	}
 }
-
-void fcfs(int arr[],int n,int p,int max){
+void fcfs(int arr[],int n,int p,int max)
+{
 	printf("\nFCFS Disk Scheduling\n");
 	int seekTime=0;
 	printf("%d => ",p);
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n;i++)
+	{
 		seekTime += getSeekTime(p,arr[i]);
 		if(i!=n-1)
 			printf("%d => ",arr[i]);
@@ -45,24 +50,24 @@ void fcfs(int arr[],int n,int p,int max){
 	}
 	printf("\nTotal Head Movements in FCFS = %d\n\n",seekTime);
 }
-
-void scan(int arr[],int n,int p,int max){
+void scan(int arr[],int n,int p,int max)
+{
 	printf("\nSCAN Disk Scheduling\n");
 	int seekTime=0,temp[n],i;
 	copy(arr,temp,n);
 	sort(temp,n);
 	int index = reqIndex(temp,n,p);
-
 	seekTime+=getSeekTime(p,max);
-
 	printf("%d => ",p);
-	for( i=index;i<n;i++){
+	for( i=index;i<n;i++)
+	{
 		printf("%d => ",temp[i]);
 		p=temp[i];
 	}
 	printf("%d\n%d => ",max,max);
 	p=max;
-	for( i=index-1;i>=0;i--){
+	for( i=index-1;i>=0;i--)
+	{
 		if(i!=0)
 			printf("%d => ",temp[i]);
 		else
@@ -72,24 +77,24 @@ void scan(int arr[],int n,int p,int max){
 	seekTime+=getSeekTime(max,temp[0]);
 	printf("\nTotal Head Movements in SCAN = %d\n\n",seekTime);
 }
-
-void cscan(int arr[],int n,int p,int max){
+void cscan(int arr[],int n,int p,int max)
+{
 	printf("\nCSCAN Disk Scheduling\n");
 	int seekTime=0,temp[n],i;
 	copy(arr,temp,n);
 	sort(temp,n);
 	int index = reqIndex(temp,n,p);
-
 	seekTime+=getSeekTime(p,max);
-
 	printf("%d => ",p);
-	for( i=index;i<n;i++){
+	for( i=index;i<n;i++)
+	{
 		printf("%d => ",temp[i]);
 		p=temp[i];
 	}
 	printf("%d \n0 => ",max);
 	p=0;
-	for( i=0;i<index;i++){
+	for( i=0;i<index;i++)
+	{
 		if(i!=index-1)
 			printf("%d => ",temp[i]);
 		else
@@ -102,7 +107,8 @@ void cscan(int arr[],int n,int p,int max){
 	printf("\nTotal Head Movements in CSCAN = %d\n\n",seekTime);
 }
 
-void main(){
+void main()
+{
 	printf("DISK SCHEDULING\n");
 	int n,position,i,min,max,p;
 	printf("Enter the disk track limit(max): ");
@@ -111,21 +117,23 @@ void main(){
 	scanf("%d",&n);
 	printf("Enter the requests \n");
 	int arr[n];
-	for(i=0;i<n;i++){
+	for(i=0;i<n;i++)
+	{
 		scanf("%d",&arr[i]);
 	}
 	printf("Enter the current position of disk arm: ");
 	scanf("%d",&p);
 	int choice;
-	do{
+	do
+	{
 		printf("1. FCFS\n");
 		printf("2. SCAN\n");
 		printf("3. CSCAN\n");
 		printf("4. Exit\n");
 		printf("Enter your choice: ");
 		scanf("%d",&choice);
-
-		switch(choice){
+		switch(choice)
+		{
 			case 1: 
 				fcfs(arr,n,p,max);
 				break;
